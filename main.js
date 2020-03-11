416faf59d03199152a82b4bd63d94c499e0d77fc
Colin McCulloch <colinmucculloch@gmail.com>
2020-03-11 10:52:12 -0400

    final version,NewGame and score implemented

--- main.js
+++ main.js
@@ -11,22 +11,50 @@ let winners = [
     [1, 5, 9]
 ]
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let game = true
+player1Score = 0
+player2Score = 0
 
 function setup() {
-    var title = document.createElement("h1")
+    var title = document.createElement("h1");
     title.innerText = "Tic Tac Toe";
-    title.className = "col-12 display-3 text-center"
+    title.className = "col-12 display-3 text-center";
     document.body.appendChild(title);
-    var player = document.createElement("div")
-    player.className = "text-center"
+    var player = document.createElement("div");
+    player.className = "text-center";
     var currentPlayer = document.createElement("p");
     currentPlayer.innerText = "Player O's turn"
-    player.appendChild(currentPlayer)
-    document.body.appendChild(player)
+    player.appendChild(currentPlayer);
+    document.body.appendChild(player);
+    var scoreDiv = document.createElement("div");
+    var scoreRow = document.createElement("row");
+    scoreRow.className = "row justify-content-center";
+    var score1Div = document.createElement("div");
+    var score2Div = document.createElement("div");
+    score1Div.className = "Col-4 m-2"
+    score2Div.className = "Col-4 m-2"
+    var score1 = document.createElement("p");
+    var score2 = document.createElement("p");
+    score1.innerText = "Player 1: " + player1Score
+    score2.innerText = "Player 2: " + player2Score
+    var btn = document.createElement("div")
+    btn.className = "Col-4 m-2"
+    var reset = document.createElement("button")
+    reset.className = "btn-dark"
+    reset.innerText = "New Game"
+    btn.onclick = newGame
+    btn.appendChild(reset)
+    score1Div.appendChild(score1)
+    score2Div.appendChild(score2)
+    scoreRow.appendChild(score1Div)
+    scoreRow.appendChild(btn)
+    scoreRow.appendChild(score2Div)
+    scoreDiv.appendChild(scoreRow)
+    document.body.appendChild(scoreDiv)
+        // creates the game board
     var game = document.createElement("div");
-    // creates the game board
     currentPlayer.setAttribute("id", "currentPlayer")
-    game.className = "game my-5 col-12 container-fluid";
+    game.className = "game my-3 col-12 container-fluid";
     let x = 1;
     for (let i = 0; i < 3; i++) {
         var row = document.createElement("div");
@@ -38,6 +66,7 @@ function setup() {
             x++
             col.onclick = toggleTurn
             var para = document.createElement("p")
+            col.style.backgroundColor = "white"
             col.appendChild(para)
             row.appendChild(col);
         }
@@ -60,19 +89,34 @@ function startOver() {
     // console.log("hey")
     document.body.innerHTML = " "
     count = 1
+    game = true
+    setup()
+}
+
+function newGame() {
+    // console.log("hey")
+    document.body.innerHTML = " "
+    count = 1
+    game = true
+    player1Score = 0
+    player2Score = 0
     setup()
 }
 
 function toggleTurn(e) {
     var playerChoice = document.getElementById(e.target.id);
     if (count % 2 == 0) {
-        // console.log('o')ZZZ
+        // console.log('o')
         currentPlayer.innerText = "Player O's turn"
         playerChoice.innerHTML = "X";
     } else {
         // console.log('x')
-        currentPlayer.innerText = "Player X's turn"
-        playerChoice.innerHTML = "O";
+        // let aiMove = document.getElementById(Math.floor(Math.random() * 9));
+        // aiMove.innerText = "X"
+        // count++
+
+        currentPlayer.innerText = "Player X's turn";
+        playerChoice.innerHTML = "O";;
     }
     count++
     // who won, is it a win or a tie?
@@ -85,6 +129,12 @@ function toggleTurn(e) {
             check2.textContent === check3.textContent &&
             check3.textContent === "X") {
             currentPlayer.innerText = "congrats X won"
+            check1.style.backgroundColor = "blue"
+            check2.style.backgroundColor = "blue"
+            check3.style.backgroundColor = "blue"
+            game = false
+            player2Score++
+
             let j = 0
             while (j < arr.length) {
                 document.getElementById(arr[j]).onclick = " "
@@ -95,12 +145,18 @@ function toggleTurn(e) {
             check2.textContent === check3.textContent &&
             check3.textContent === "O") {
             currentPlayer.innerText = "congrats O won"
+            game = false
+            player1Score++
+            check1.style.backgroundColor = "blue"
+            check2.style.backgroundColor = "blue"
+            check3.style.backgroundColor = "blue"
             let j = 0
             while (j < arr.length) {
                 document.getElementById(arr[j]).onclick = " "
                 j++
             }
-        } else if (count == 10) {
+            break;
+        } else if (count == 10 && game) {
             currentPlayer.innerText = "congrats it's a tie"
         }
     }
